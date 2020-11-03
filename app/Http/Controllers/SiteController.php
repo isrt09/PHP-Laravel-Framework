2c09eb0a842600ba13d02e9800ee4f9cddb4d839
isrt09 <mazedur@gmail.com>
2020-11-03 11:49:29 +0600

    Pages setup

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function showHome(){
+    	return view('home');
+    }
+
+    public function showAbout(){
+    	return view('about');
+    }
+
+    public function showService(){
+    	return view('service');
+    }
+
+    public function showPortfolio(){
+    	return view('portfolio');
+    }
+}
