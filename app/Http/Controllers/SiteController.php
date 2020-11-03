c96f61f0f9177fbb2eda68133856d448a6c9a596
isrt09 <mazedur@gmail.com>
2020-11-03 13:06:32 +0600

    Project setup is done

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -9,15 +9,12 @@ class SiteController extends Controller
     public function showHome(){
     	return view('home');
     }
-
     public function showAbout(){
     	return view('about');
     }
-
     public function showService(){
     	return view('service');
     }
-
     public function showPortfolio(){
     	return view('portfolio');
     }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-
 Route::get('/','SiteController@showHome');
 Route::get('/about','SiteController@showAbout');
 Route::get('/service','SiteController@showService');
 Route::get('/portfolio','SiteController@showPortfolio');
-
